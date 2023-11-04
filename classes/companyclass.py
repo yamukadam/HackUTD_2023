16887ee24cacc9bcd9f8411c82cb154f3b92c18a
humzaaqureshi <114468040+humzaaqureshi@users.noreply.github.com>
2023-11-04 18:31:39 -0500

    Add files via upload

---
+++ classes/companyclass.py
@@ -0,0 +1,16 @@
+class company:
+    def __init__(self, name, country, politics = None, violence = None):
+        self.name = name
+        self.country = country
+        self.politics = politics
+        self.violence = violence
+    def name(self):
+        return self.name
+    def country(self):
+        return self.country
+    def politics(self):
+        return self.politics
+    def violence(self):
+        return self.violence
+    
+
