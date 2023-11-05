5fad91ca77fcfe96a5e29a234fa8b7302dd9ac59
Yaeesh <mukadam.yaeesh@gmail.com>
2023-11-05 01:57:16 -0500

    testing turning main into function

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ prompter()
 load_dotenv()
 api_key = os.environ['API_KEY']
 openai.api_key = api_key
-messages = [ {"role": "system", "content":  
-              "You are a intelligent assistant."} ] 
+def run():
+    messages = [ {"role": "system", "content":  
+                "You are a intelligent assistant."} ] 
 
-companies = ["Exxon", "Saudi Aramco", "Shell"]
-for company in companies:
-    promptfile = open(f'{company}prompt.txt','r')
-    message = promptfile.read()
-    if message: 
-        messages.append( 
-            {"role": "user", "content": message}, 
-        ) 
-        chat = openai.ChatCompletion.create( 
-            model="gpt-3.5-turbo-16k", messages=messages 
-        ) 
-    reply = chat.choices[0].message.content 
-    print(f"ChatGPT: {reply}") 
-    messages.append({"role": "assistant", "content": reply})
-    print("")
+    companies = ["Exxon", "Saudi Aramco", "Shell"]
+    for company in companies:
+        promptfile = open(f'{company}prompt.txt','r')
+        message = promptfile.read()
+        if message: 
+            messages.append( 
+                {"role": "user", "content": message}, 
+            ) 
+            chat = openai.ChatCompletion.create( 
+                model="gpt-3.5-turbo-16k", messages=messages 
+            ) 
+        reply = chat.choices[0].message.content 
+        print(f"ChatGPT: {reply}") 
+        messages.append({"role": "assistant", "content": reply})
+        print("")
